822634b18a1a213fcf632f0d443ba997df3fa52b
myvnc <87856062+myvnc@users.noreply.github.com>
2021-08-15 00:45:35 +0800

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -72,9 +72,7 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
-              //<Link className="navbar-item" to="/contact/examples">
-              // 
-              //</Link>
+             
             </div>
             <div className="navbar-end has-text-centered">
               <a
